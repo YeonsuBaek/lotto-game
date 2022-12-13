2ab64977902bc727c76e79d9c34dbc510abd6b33
YeonsuBaek <soothbugger@gmail.com>
2022-12-13 22:30:12 +0900

    feat(lotto2): 당첨 번호 입력에 대한 예외 처리 기능 구현

--- src/retry/Lotto.js
+++ src/retry/Lotto.js
@@ -2,7 +2,41 @@ class Lotto {
   #numbers;
 
   constructor(numbers) {
+    this.validate(numbers);
     this.#numbers = numbers;
+  }
+
+  validate(numbers) {
+    this.validateType(numbers);
+    this.validateLength(numbers);
+    this.validateRange(numbers);
+    this.validateDuplicate(numbers);
+  }
+
+  validateType(numbers) {
+    const notNumbers = numbers.filter((number) => {
+      return isNaN(number);
+    });
+    if (notNumbers.length > 0) throw new Error('[ERROR] 당첨 번호는 숫자로 이루어져야 합니다.');
+  }
+
+  validateLength(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
+    }
+  }
+
+  validateRange(numbers) {
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) throw new Error('[ERROR] 당첨 번호는 1과 45 사이 숫자여야 합니다.');
+    });
+  }
+
+  validateDuplicate(numbers) {
+    const deletedDuplicate = new Set(numbers);
+    if (deletedDuplicate.size !== numbers.length) {
+      throw new Error('[ERROR] 당첨 번호는 중복되지 않아야 합니다.');
+    }
   }
 
   getWinningNumbers() {
