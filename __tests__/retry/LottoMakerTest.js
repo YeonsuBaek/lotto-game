e86417d9d121b65ba87a82368de914bf9916bbde
YeonsuBaek <soothbugger@gmail.com>
2022-12-13 21:48:14 +0900

    test(lotto2): 로또 번호 발행에 대한 테스트 구현 완료

---
+++ __tests__/retry/LottoMakerTest.js
@@ -0,0 +1,22 @@
+const LottoMaker = require('../../src/retry/LottoMaker');
+
+describe('로또 번호 발행 테스트', () => {
+  test('구매 수량만큼의 로또를 생성한다.', () => {
+    const lottos = LottoMaker.generate(5);
+    expect(lottos.length).toBe(5);
+  });
+
+  test('각각의 로또는 중복되는 숫자가 없으며 길이는 6이다.', () => {
+    const lottos = LottoMaker.generate(5);
+    expect(() => {
+      lottos
+        .forEach((lotto) => {
+          const lottoDeletedDuplicate = new Set(lotto);
+          if (lottoDeletedDuplicate.size !== 6) {
+            throw new Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+          }
+        })
+        .toBeTruthy();
+    });
+  });
+});
